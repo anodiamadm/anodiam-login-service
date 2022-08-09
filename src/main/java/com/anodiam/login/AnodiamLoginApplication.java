446e9883c9b4ce7faf16cc1078fdc6977086a376
subrata-hazra <subratah@gmail.com>
2022-08-09 22:08:43 +0530

    Removed secrets.yaml and migrated all secrets to GCP secret manager

--- src/main/java/com/anodiam/login/AnodiamLoginApplication.java
+++ src/main/java/com/anodiam/login/AnodiamLoginApplication.java
@@ -1,5 +1,6 @@
 package com.anodiam.login;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -7,6 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
 @EnableKafka
 public class AnodiamLoginApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AnodiamLoginApplication.class, args);
     }
